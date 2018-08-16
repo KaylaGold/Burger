3a6c0209fd29c9537838fd655e7f62e6352faa43
Kayla Goldner <35668814+KaylaGold@users.noreply.github.com>
2018-08-16 10:15:05 -0700

    Update seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
 INSERT INTO burgers (burger_name) VALUES ('Cheese Bacon Burger');
-INSERT INTO burgers (burger_name, devoured) VALUES ('Quarter Pounder with Cheese', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Jalepeno Burger', true); 
+INSERT INTO burgers (burger_name, devoured) VALUES ('Quarter Pounder with Cheese');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Jalepeno Burger'); 
