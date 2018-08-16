45dd432e766abfa756e16b38ff87cda56af4311a
Kayla Goldner <35668814+KaylaGold@users.noreply.github.com>
2018-08-16 10:09:45 -0700

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,12 @@
 // Set up MySQL connection.
 let mysql = require("mysql");
 
-let connection = mysql.createConnection({
+let connection; 
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
@@ -9,13 +14,15 @@ let connection = mysql.createConnection({
     database: "burgers_db"
 });
 
+};
+
 // Make connection.
 connection.connect(function (err) {
     if (err) {
         console.error("error connecting: " + err.stack);
         return;
     }
-    console.log("connected as id " + connection.threadId);
+    console.log("Cnnected as id " + connection.threadId);
 });
 
 // Export connection for our ORM to use.
