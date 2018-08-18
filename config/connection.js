3feb5f6978918b7cfd4f1db7097ea00e69cb4c9f
Kayla Goldner <kaylamariegoldner1@Kaylas-MacBook-Air.local>
2018-08-17 19:15:03 -0700

    commit

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ if (process.env.JAWSDB_URL) {
 } else {
   connection = mysql.createConnection({
     host: "localhost",
-    port: 15974,
+    port: 3306,
     user: "root",
     password: "",
     database: "burgers_db"
